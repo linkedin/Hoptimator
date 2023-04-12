fa336623ea8fb5fd1dfab5c6751d1eb075f3f8d3
Ryanne Dolan <rdolan@linkedin.com>
2023-04-12 14:24:40 -0500

    Operator with KafkaTopic controller

---
+++ models/generate-models.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+docker pull ghcr.io/kubernetes-client/java/crd-model-gen:v1.0.6
+
+docker run \
+  --rm \
+  --mount type=bind,src=/var/run/docker.sock,dst=/var/run/docker.sock \
+  --mount type=bind,src="$(pwd)",dst="$(pwd)" \
+  -ti \
+  --network host \
+  ghcr.io/kubernetes-client/java/crd-model-gen:v1.0.6 \
+  /generate.sh -o "$(pwd)/models" -n "" -p "com.linkedin.hoptimator" \
+  -u "$(pwd)/deploy/kafkatopics.crd.yaml" \
+  && echo "done."
