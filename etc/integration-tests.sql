d969aa083c22f9ee36a6b652640ae7e8d4a8db8f
Ryanne Dolan <rdolan@linkedin.com>
2023-05-11 14:59:28 -0500

    Add simple MySQL CDC integration test

--- etc/integration-tests.sql
+++ etc/integration-tests.sql
@@ -1,10 +1,15 @@
 
-!connect "jdbc:calcite:model=./model.yaml" "" ""
-
 !set maxWidth 80
 !table
 !schemas
 
+-- built-in bounded tables
 SELECT * FROM DATAGEN.PERSON;
 SELECT * FROM DATAGEN.COMPANY;
+
+-- MySQL CDC tables
+SELECT * FROM INVENTORY."products_on_hand" LIMIT 1;
+
+-- MySQL CDC -> Kafka
+SELECT * FROM RAWKAFKA."products" LIMIT 1;
 
--- etc/readiness-probe.sql
+++ etc/readiness-probe.sql
@@ -1,10 +1,8 @@
 
-!connect "jdbc:calcite:model=./model.yaml" "" ""
-
-!set maxWidth 80
-!table
-!schemas
+!connect "jdbc:calcite:model=/etc/config/model.yaml" "" ""
 
 SELECT * FROM DATAGEN.PERSON;
 SELECT * FROM DATAGEN.COMPANY;
+SELECT * FROM INVENTORY."products_on_hand" LIMIT 1;
+SELECT * FROM RAWKAFKA."products" LIMIT 1;
 
