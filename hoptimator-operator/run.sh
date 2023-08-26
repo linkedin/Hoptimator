8254974119d56f4df941416225ff9ee8486d899f
Ryanne Dolan <ryannedolan@gmail.com>
2023-08-25 23:35:33 -0500

    Drop fat application jars

--- hoptimator-operator/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-java \
-  --add-opens java.base/java.lang=ALL-UNNAMED \
-  --add-opens java.base/java.util=ALL-UNNAMED \
-  --add-opens java.base/java.time=ALL-UNNAMED \
-  -classpath "/opt/plugins/*/lib/*:./hoptimator-operator-all.jar" \
-  $JAVA_OPTS \
-  com.linkedin.hoptimator.operator.HoptimatorOperatorApp "$@"
