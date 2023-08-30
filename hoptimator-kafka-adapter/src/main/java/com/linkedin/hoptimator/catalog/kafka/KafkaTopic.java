027347b0b0ad9bd8a58b14e4b7a38c52ae67cb7c
Ryanne Dolan <ryannedolan@gmail.com>
2023-08-30 16:32:16 -0500

    Bug fix: handle capitals in Kafka topics

--- hoptimator-kafka-adapter/src/main/java/com/linkedin/hoptimator/catalog/kafka/KafkaTopic.java
+++ hoptimator-kafka-adapter/src/main/java/com/linkedin/hoptimator/catalog/kafka/KafkaTopic.java
@@ -2,14 +2,14 @@ package com.linkedin.hoptimator.catalog.kafka;
 
 import com.linkedin.hoptimator.catalog.Resource;
 
-import java.util.Optional;
-import java.util.Collections;
+import java.util.Locale;
 import java.util.Map;
 
 class KafkaTopic extends Resource {
-  public KafkaTopic(String topicName, Map<String, String> clientOverrides) {
+  KafkaTopic(String topicName, Map<String, String> clientOverrides) {
     super("KafkaTopic");
     export("topicName", topicName);
+    export("topicNameLowerCase", topicName.toLowerCase(Locale.ROOT));
     export("clientOverrides", clientOverrides);
   }
 }
