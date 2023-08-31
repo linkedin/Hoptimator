5dc0c2ba5dd874f488df96455a13a6d2dc2bb8c5
Ryanne Dolan <ryannedolan@gmail.com>
2023-08-31 15:44:01 -0500

    Bug fix: handle capitals in Kafka topics (#50)

--- hoptimator-kafka-adapter/src/main/java/com/linkedin/hoptimator/catalog/kafka/KafkaTopicAcl.java
+++ hoptimator-kafka-adapter/src/main/java/com/linkedin/hoptimator/catalog/kafka/KafkaTopicAcl.java
@@ -2,10 +2,13 @@ package com.linkedin.hoptimator.catalog.kafka;
 
 import com.linkedin.hoptimator.catalog.Resource;
 
+import java.util.Locale;
+
 class KafkaTopicAcl extends Resource {
   public KafkaTopicAcl(String topicName, String principal, String method) {
     super("KafkaTopicAcl");
     export("topicName", topicName);
+    export("topicNameLowerCase", topicName.toLowerCase(Locale.ROOT));
     export("principal", principal);
     export("method", method);
   }
