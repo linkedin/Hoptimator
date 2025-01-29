61038e2b57f73f3fd96388a70267f1c4c2991f78
Joseph Grogan <jogrogan@linkedin.com>
2025-01-29 15:06:56 -0500

    checkstyle fixes

--- hoptimator-util/src/main/java/com/linkedin/hoptimator/util/planner/HoptimatorJdbcTableScan.java
+++ hoptimator-util/src/main/java/com/linkedin/hoptimator/util/planner/HoptimatorJdbcTableScan.java
@@ -1,20 +1,11 @@
 package com.linkedin.hoptimator.util.planner;
 
-import org.apache.calcite.adapter.jdbc.JdbcTable;
-import org.apache.calcite.adapter.jdbc.JdbcTableScan;
-import org.apache.calcite.plan.Convention;
-import org.apache.calcite.plan.RelOptCluster;
-import org.apache.calcite.plan.RelOptTable;
-import org.apache.calcite.plan.RelTraitSet;
-import org.apache.calcite.rel.RelNode;
-import org.apache.calcite.rel.core.TableScan;
-import org.apache.calcite.rel.hint.RelHint;
-
-import com.google.common.collect.ImmutableList;
-
 import java.util.List;
 
-import static org.apache.calcite.linq4j.Nullness.castNonNull;
+import org.apache.calcite.adapter.jdbc.JdbcTableScan;
+import org.apache.calcite.plan.RelOptCluster;
+import org.apache.calcite.plan.RelOptTable;
+import org.apache.calcite.rel.hint.RelHint;
 
 
 public class HoptimatorJdbcTableScan extends JdbcTableScan {
