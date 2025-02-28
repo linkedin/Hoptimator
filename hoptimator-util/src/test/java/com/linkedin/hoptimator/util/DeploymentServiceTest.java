9eb834df7d22afa9c0c55573ab48180645284f0e
Eric Honer <ehoner@gmail.com>
2025-02-27 16:50:18 -0800

    allow flag compatible parsing

--- hoptimator-util/src/test/java/com/linkedin/hoptimator/util/DeploymentServiceTest.java
+++ hoptimator-util/src/test/java/com/linkedin/hoptimator/util/DeploymentServiceTest.java
@@ -1,5 +1,9 @@
 package com.linkedin.hoptimator.util;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 
@@ -9,14 +13,15 @@ import static com.linkedin.hoptimator.util.DeploymentService.HINT_OPTION;
 import static com.linkedin.hoptimator.util.DeploymentService.PIPELINE_OPTION;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 
 class DeploymentServiceTest {
 
   /**
-   * "hint" keys <b>and</b> values are required to be non-{@code null}. A
-   * {@code null} or empty {@link Map} are considered invalid and should
-   * <b>not</b> be added to the {@link Properties} object.
+   * "hint" keys <b>and</b> values are required to be non-{@code null}. An
+   * empty {@link Map} is considered invalid and should <b>not</b> be added
+   * to the {@link Properties} object.
    * <br/>
    * nb. "pipeline" values are <b>always</b> added when present.
    */
@@ -25,18 +30,17 @@ class DeploymentServiceTest {
     Map<String, String> empty = DeploymentService.parseHints(new Properties());
     assertTrue(empty.isEmpty(), "An empty map should not add `hints`.");
 
-    Map<String, String> nokey = DeploymentService.parseHints(new Properties() {{
-      put(HINT_OPTION, "");
-    }});
-    assertTrue(nokey.isEmpty(), "A map without keys should not add `hints`.");
-
     Map<String, String> defined = DeploymentService.parseHints(new Properties() {{
       put(HINT_OPTION, "key=value");
     }});
     assertEquals("value", defined.get("key"), "Did not match expected key value pair: `key=value`.");
 
+    Map<String, String> nokey = DeploymentService.parseHints(new Properties() {{
+      put(HINT_OPTION, "flag0=,flag1=");
+    }});
+    assertTrue(nokey.keySet().containsAll(Arrays.asList("flag0", "flag1")), "Expected to find flags.");
+
     Map<String, String> pipelineOnly = DeploymentService.parseHints(new Properties() {{
-      putAll(nokey);
       put(PIPELINE_OPTION, "pipeline");
     }});
     assertEquals("pipeline", pipelineOnly.get(PIPELINE_OPTION), "Did not match expected `pipeline` value.");
