34417847332ecb32c6c6f92a2791e66900f22ea3
Shrinand Thakkar <sthakkar@linkedin.com>
2025-07-24 15:47:01 -0700

    Rebase Master

--- hoptimator-k8s/src/main/java/com/linkedin/hoptimator/k8s/K8sSourceDeployer.java
+++ hoptimator-k8s/src/main/java/com/linkedin/hoptimator/k8s/K8sSourceDeployer.java
@@ -2,12 +2,11 @@ package com.linkedin.hoptimator.k8s;
 
 import java.sql.SQLException;
 import java.util.List;
-import java.util.Map;
 import java.util.Objects;
-import java.util.Properties;
 import java.util.stream.Collectors;
 
 import com.linkedin.hoptimator.Source;
+import com.linkedin.hoptimator.jdbc.HoptimatorConnection;
 import com.linkedin.hoptimator.k8s.models.V1alpha1TableTemplate;
 import com.linkedin.hoptimator.k8s.models.V1alpha1TableTemplateList;
 import com.linkedin.hoptimator.k8s.models.V1alpha1TableTemplateSpec;
@@ -16,13 +15,13 @@ import com.linkedin.hoptimator.util.Template;
 
 /** Specifies an abstract Source with concrete YAML by applying TableTemplates. */
 class K8sSourceDeployer extends K8sYamlDeployer {
-  private final Map<String, String> hints;
+  private final K8sContext context;
   private final Source source;
   private final K8sApi<V1alpha1TableTemplate, V1alpha1TableTemplateList> tableTemplateApi;
 
-  K8sSourceDeployer(Source source, K8sContext context, Properties connectionProperties) {
+  K8sSourceDeployer(Source source, K8sContext context) {
     super(context);
-    this.hints = DeploymentService.parseHints(connectionProperties);
+    this.context = context;
     this.source = source;
     this.tableTemplateApi = new K8sApi<>(context, K8sApiEndpoints.TABLE_TEMPLATES);
   }
@@ -30,25 +29,27 @@ class K8sSourceDeployer extends K8sYamlDeployer {
   @Override
   public List<String> specify() throws SQLException {
     String name = K8sUtils.canonicalizeName(source.database(), source.table());
-    Template.Environment env =
-        new Template.SimpleEnvironment()
-            .with("name", name)
-            .with("database", source.database())
-            .with("schema", source.schema())
-            .with("table", source.table())
-            .with(source.options())
-            .with(hints);
+    try (HoptimatorConnection connection = context.connection()) {
+      Template.Environment env =
+          new Template.SimpleEnvironment()
+              .with("name", name)
+              .with("database", source.database())
+              .with("schema", source.schema())
+              .with("table", source.table())
+              .with(source.options())
+              .with(DeploymentService.parseHints(connection.connectionProperties()));
 
-    return tableTemplateApi.list()
-        .stream()
-        .map(V1alpha1TableTemplate::getSpec)
-        .filter(Objects::nonNull)
-        .filter(x -> x.getDatabases() == null || x.getDatabases().contains(source.database()))
-        .filter(x -> x.getMethods() == null || x.getMethods().contains(K8sUtils.method(source)))
-        .map(V1alpha1TableTemplateSpec::getYaml)
-        .filter(Objects::nonNull)
-        .map(x -> new Template.SimpleTemplate(x).render(env))
-        .filter(Objects::nonNull) // Filter out null templates (which might have errored out due to missing hint expressions)
-        .collect(Collectors.toList());
+      return tableTemplateApi.list()
+          .stream()
+          .map(V1alpha1TableTemplate::getSpec)
+          .filter(Objects::nonNull)
+          .filter(x -> x.getDatabases() == null || x.getDatabases().contains(source.database()))
+          .filter(x -> x.getMethods() == null || x.getMethods().contains(K8sUtils.method(source)))
+          .map(V1alpha1TableTemplateSpec::getYaml)
+          .filter(Objects::nonNull)
+          .map(x -> new Template.SimpleTemplate(x).render(env))
+          .filter(Objects::nonNull) // Filter out null templates (which might have errored out due to missing hint expressions)
+          .collect(Collectors.toList());
+    }
   }
 }
