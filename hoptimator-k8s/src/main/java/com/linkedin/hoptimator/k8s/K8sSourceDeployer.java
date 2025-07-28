a1e9bf89cd1db1e8b07d80b92d77a4ceaae42449
Shrinand Thakkar <sthakkar@linkedin.com>
2025-07-28 14:36:14 -0700

    Do not use try with resources, otherwise, the resource is closed at the block end

--- hoptimator-k8s/src/main/java/com/linkedin/hoptimator/k8s/K8sSourceDeployer.java
+++ hoptimator-k8s/src/main/java/com/linkedin/hoptimator/k8s/K8sSourceDeployer.java
@@ -29,27 +29,26 @@ class K8sSourceDeployer extends K8sYamlDeployer {
   @Override
   public List<String> specify() throws SQLException {
     String name = K8sUtils.canonicalizeName(source.database(), source.table());
-    try (HoptimatorConnection connection = context.connection()) {
-      Template.Environment env =
-          new Template.SimpleEnvironment()
-              .with("name", name)
-              .with("database", source.database())
-              .with("schema", source.schema())
-              .with("table", source.table())
-              .with(source.options())
-              .with(DeploymentService.parseHints(connection.connectionProperties()));
+    HoptimatorConnection connection = context.connection();
+    Template.Environment env =
+        new Template.SimpleEnvironment()
+            .with("name", name)
+            .with("database", source.database())
+            .with("schema", source.schema())
+            .with("table", source.table())
+            .with(source.options())
+            .with(DeploymentService.parseHints(connection.connectionProperties()));
 
-      return tableTemplateApi.list()
-          .stream()
-          .map(V1alpha1TableTemplate::getSpec)
-          .filter(Objects::nonNull)
-          .filter(x -> x.getDatabases() == null || x.getDatabases().contains(source.database()))
-          .filter(x -> x.getMethods() == null || x.getMethods().contains(K8sUtils.method(source)))
-          .map(V1alpha1TableTemplateSpec::getYaml)
-          .filter(Objects::nonNull)
-          .map(x -> new Template.SimpleTemplate(x).render(env))
-          .filter(Objects::nonNull) // Filter out null templates (which might have errored out due to missing hint expressions)
-          .collect(Collectors.toList());
-    }
+    return tableTemplateApi.list()
+        .stream()
+        .map(V1alpha1TableTemplate::getSpec)
+        .filter(Objects::nonNull)
+        .filter(x -> x.getDatabases() == null || x.getDatabases().contains(source.database()))
+        .filter(x -> x.getMethods() == null || x.getMethods().contains(K8sUtils.method(source)))
+        .map(V1alpha1TableTemplateSpec::getYaml)
+        .filter(Objects::nonNull)
+        .map(x -> new Template.SimpleTemplate(x).render(env))
+        .filter(Objects::nonNull) // Filter out null templates (which might have errored out due to missing hint expressions)
+        .collect(Collectors.toList());
   }
 }
